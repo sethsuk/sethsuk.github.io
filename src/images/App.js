502d3f6adb9911ea7e07723995616a8890693c52
Seth Sukboontip <sukboontips@gmail.com>
2024-11-02 16:40:08 -0400

    refactored file structure w/ React

---
+++ src/images/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+        <header>
+            <div class="header-banner">
+                <ul class="header-links">
+                    <li><a href="mailto:sethsuk@seas.upenn.edu" target="_blank">Email</a></li>
+                    <li><a href="https://www.linkedin.com/in/seth-sukboontip/" target="_blank">LinkedIn</a></li>
+                    <li><a href="https://github.com/sethsuk" target="_blank">GitHub</a></li>
+                    <li><a href="https://www.instagram.com/kilroys_magical_world/" target="_blank">📷</a></li>
+                </ul>
+            </div>
+        </header>
+
+        <main class="container">
+            <div class="left-section">
+                <div class="name">
+                    <h1>Seth Sukboontip</h1>
+                    <h3>Computer Science, BSE @ UPenn '27</h3>
+                </div>
+                <div class="info">
+                    <P><strong>Hometown:</strong> Bangkok, Thailand</P>
+                    <p><strong>Skill Set:</strong> Backend development, Databases, RESTful APIs</p>
+                    <p><Strong>Passions:</Strong> Digital & film photography, Drumming, Thai basil crispy pork belly, Late-night izakaya runs</p>
+                    <p><Strong>Current Interest Phase:</Strong> Budget audiophile, Custom mechanical keyboards, Photo book collages</p>
+                </div>
+            </div>
+            <div class="pfp">
+                <img src="images/seth_sukboontip_image.jpg" alt="Headshot of Seth Sukboontip"/>
+            </div>
+        </main>
+
+        <footer>
+            <p>&copy; 2024 Seth Sukboontip. All rights reserved.</p>
+        </footer>
+    </div>
+  );
+};
+
+export default App;
