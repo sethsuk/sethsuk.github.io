a8cbbc3a748a87ce09ec3930465ad7134c9dce99
Seth Sukboontip <sethsuk@seas.upenn.edu>
2026-07-18 22:10:38 -0400

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Nav from '@/components/Nav'
 import Footer from '@/components/Footer'
 import PageTransition from '@/components/PageTransition'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -82,6 +83,7 @@ export default function RootLayout({
         </main>
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
